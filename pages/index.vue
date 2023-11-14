96714da8b0a790f65f26cdf273e4bebf13abe111
George Nick Gorzynski <georgegorzynski@me.com>
2023-11-14 13:35:24 +0000

    Replace index.vue with new version.

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,11 @@
 <template>
-  <div class="min-h-screen bg-background flex flex-col gap-16 justify-center">
-    <NuxtImg src="/surtes-logo.svg" class="mx-auto px-12 w-full max-w-2xl" />
-    <h1 class="text-center">Coming Soon...</h1>
-  </div>
+  <HomepageHero />
+  <HomepageAchievements />
+  <HomepageContactUs />
 </template>
 
 <script setup lang="ts">
 definePageMeta({
-  title: "Team SURTES"
+  title: "Home",
 });
 </script>
