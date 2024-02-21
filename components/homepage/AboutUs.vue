230e236e7f3d29171fae13c4a62d03c98ea7a084
t0yamont <montillafelipe072@gmail.com>
2024-02-21 17:09:06 +0000

    Changes to navigation, hero and aboutus

--- components/homepage/AboutUs.vue
+++ components/homepage/AboutUs.vue
@@ -3,7 +3,7 @@
     <div id="about-us-inner" class="max-w-full lg:max-w-6xl mx-auto py-12 px-8">
       <h2 class="mb-4">Who are we?</h2>
       <div class="flex flex-col md:flex-row gap-4 md:gap-8 py-4 items-center">
-        <p>Founded in 2009 at the University of Surrey, we're a group of motorsport enthusiasts working together to compete at the <NuxtLink to="https://www.imeche.org/events/formula-student" target="_blank" rel="noopener">IMechE Formula Student competition</NuxtLink> at the world acclaimed Silverstone track.</p>
+        <p>Founded in 2009 at the University of Surrey, we're a group of motorsport enthusiasts working together to compete at the <NuxtLink to="https://www.imeche.org/events/formula-student" class="text-decoration-line: underline" target="_blank" rel="noopener">IMechE Formula Student competition</NuxtLink> at the world acclaimed Silverstone track.</p>
         <NuxtImg src="/img/fs-se4.webp" class="w-full rounded-lg justify-self-center max-md:order-first" />
       </div>
       <div class="flex flex-col md:flex-row gap-4 md:gap-8 py-4 items-center">
